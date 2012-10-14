7024d21ec9a27fb9672f1da7c6084851e84b35ea
shinjiwoong <mcthejjang@gmail.com>
2012-10-15 04:14:42 +0900

    프로그램 지도를 첨  다시 구성했다
    1.사각형 사이즈로 알고리즘구성
    
    세로선 가로선 그리는것도 따로 만듬
    
    기존에 배열을 이용한 그리기보단 좌표값만 줘서 박스를 만들기가 쉬워졌음

---
+++ color.h
@@ -0,0 +1,21 @@
+#ifndef __COLOR_H__
+#define __COLOR_H__
+
+
+#define blue 	1
+#define green 	10
+#define red	12
+#define purple	5
+
+#define	s	179 // 세로 
+#define g	196  //가로
+#define L	218 //왼쪽위  
+#define R	191 //오른쪽 위 
+#define l	192 // 왼쪽 아래 
+#define r	217 //오른쪽 아래
+#define A	195 // ㅏ
+#define U	180 //ㅓ
+#define N	194 //ㅜ
+#define H	193 //ㅗ
+		
+#endif // __COLOR_H__
