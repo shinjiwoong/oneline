169ba7b7334b57037144d53909f08f6d3496cd5f
shinjiwoong <mcthejjang@gmail.com>
2012-10-15 10:19:38 +0900

    색이랑 모양은 따로 헤더로 만들었다

--- ddr2.c
+++ ddr2.c
@@ -1,51 +1,97 @@
 #include<stdio.h>
 #include<dos.h>
 #include<conio.h>
+#include"color.h"
 
-#define	s	179 // 세로 
-#define g	196  //가로
-#define L	218 //왼쪽위  
-#define R	191 //오른쪽 위 
-#define l	192 // 왼쪽 아래 
-#define r	217 //오른쪽 아래
-#define A	195 // ㅏ
-#define U	180 //ㅓ
 
-unsigned char far *p = (unsigned char far *)0XB8000000;
+/*그리기 함수 */
+void square(unsigned char hX, unsigned char vY, unsigned char hLen, unsigned char vLen, unsigned char form);//사각형 그리기
+void axis(unsigned char hX, unsigned char vY, unsigned char hLen, unsigned char vLen, unsigned char form);// 축그리기
+void draw(unsigned char spX, unsigned char spY, unsigned char type, unsigned char form);//화면에 그리기
+void map();// 틀 구성
 
-void paint(int x, int y, int ch ,int attr);
-void word(int x,int y,int ch,int attr);
+
 
 int main()
 { 
 	clrscr();
-	word(30,15,65,15);
-
-	paint(0,0,10,150);
+	map();
+	
 	getch();
 	return 0;
 }
-
-void paint(int x, int y, int garo ,int sero)
+void map() 
 {
-
-	for(x=0; sero>x; x++)
-	{
-		for(y=0; garo>y; y++)
-		{	
-			word(x,y,g,15);
-		
-		}
-	}
+	square(0,0,80,22,green);
+	axis(0,3,80,0,green);
+	square(5,6,70,9,red);
+	square(6,7,68,7,green);
+	square(5,17,70,4,green);
 }
-void word(int x,int y,int ch,int attr)
+
+void square(unsigned char hX, unsigned char vY, unsigned char hLen, unsigned char vLen, unsigned char form) 
 {
-		p =  (unsigned char far *)0XB8000000+ (y * 160) + (x *2);
-		*p = ch;
-		++p;
-		*p = attr;
-		++p;
+	unsigned char spx = hX;
+	unsigned char spy = vY;
+	
+		draw(spx,spy,L,form);						   // 왼쪽 상단 모서리
+		for(spx++; hX+ hLen -1> spx; spx++) // 상단 수평선 
+		{	
+			draw(spx,spy,g,form);
+		}
+		draw(spx,spy,R,form);					     // 오른쪽 상단 모서리
+		for(spy++; vY+vLen -1> spy; spy++) // 오른쪽 수직선
+		{	
+			draw(spx,spy,s,form);
+		}
+		draw(spx,spy,r,form);						// 오른쪽 하단 모서리
+		for(spx--; hX < spx; spx--) 			// 하단 수평선
+		{	
+			draw(spx,spy,g,form);
+		}
+		draw(spx,spy,l,form);							// 왼쪽 하단 모서리
+		for(spy--; vY< spy; spy--) 			// 왼쪽 수직선
+		{	
+			draw(spx,spy,s,form);
+		}
 		return;
 }
+void axis(unsigned char hX, unsigned char vY, unsigned char hLen, unsigned char vLen, unsigned char form) 
+{
+		unsigned char spx = hX;
+		unsigned char spy = vY;
+		
+		if(hX == 0)	 //가로 선
+		{
+			draw(spx,spy,A,form);						   // 왼쪽 상단 모서리
+			for(spx++; hX+ hLen -1> spx; spx++)    // 상단 수평선 
+			{	
+				draw(spx,spy,g,form);
+			}
+			draw(spx,spy,U,form);
+		}
+		if(vY == 0) // 세로선
+		{
+			draw(spx,spy,N,form);
+			for(spy++; vY+vLen -1> spy; spy++) // 오른쪽 수직선
+			{	
+			draw(spx,spy,s,form);
+			}
+			draw(spx,spy,H,form);
+		}
+		return;
+}
+void draw(unsigned char spX, unsigned char spY, unsigned char type, unsigned char form)
+{
+		unsigned char far *p;
+		p =  (unsigned char far *)0XB8000000+ (spY * 160) + (spX *2);
+		*p++ = type;
+		*p = form;
+		return;
+}
+
+
+
+
 
 
