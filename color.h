1765d26ac3261e9bc8bb8826cf3413856ca10e8c
shinjiwoong <mcthejjang@gmail.com>
2012-10-17 10:37:47 +0900

    각종 디파인값을 더 추가하였다

--- color.h
+++ color.h
@@ -1,12 +1,14 @@
 #ifndef __COLOR_H__
 #define __COLOR_H__
 
-
-#define blue 	1
-#define green 	10
-#define red	12
+/* 그리기 색  */
+#define blue		1
+#define green	10
+#define red		12
 #define purple	5
+#define white		15 
 
+/* 그리기  값 */
 #define	s	179 // 세로 
 #define g	196  //가로
 #define L	218 //왼쪽위  
@@ -17,5 +19,18 @@
 #define U	180 //ㅓ
 #define N	194 //ㅜ
 #define H	193 //ㅗ
+		
+/* 키보드 값 */
+#define right		77
+#define left		75
+#define esc		27
+#define F10		68
+
+/* 변수 값 */
+#define sp 		7
+#define move	14
+#define bar_len	7
+#define bar_po	2
+
 		
 #endif // __COLOR_H__
